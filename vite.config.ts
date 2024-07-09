3986e531718b6f5066cd9ba686b27cbc5465a5cd
tetra-min <minjun.lee@tetracom.co.jp>
2024-07-09 19:35:25 +0900

    add config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,20 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+// import path from 'path';
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default () => {
+  const dirname = import.meta.dirname;
+
+  const resolveConfig = defineConfig({
+    plugins: [
+      react()
+    ],
+    resolve: {
+      alias: {
+        '@': dirname + '/src'
+      },
+    }
+  });
+
+  return resolveConfig;
+};
