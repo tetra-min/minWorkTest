4b1217bcd38a57d46f2c4303af0e412264a5a07e
tetra-min <minjun.lee@tetracom.co.jp>
2024-07-23 12:59:10 +0900

    add build manualchunk option

--- vite.config.ts
+++ vite.config.ts
@@ -4,10 +4,35 @@ import react from "@vitejs/plugin-react-swc";
 // import path from 'path';
 
 export default () => {
-    const dirname = import.meta.dirname;
+    const dirname: string = import.meta.dirname;
+
+    const chunks: Array<string> = [];
 
     const resolveConfig = defineConfig({
         plugins: [react()],
+        optimizeDeps: {
+            include: ["react", "react-dom"],
+        },
+        build: {
+            rollupOptions: {
+                output: {
+                    manualChunks(id) {
+                        if (id.includes("node_modules/")) {
+                            const module = id.toString().split("node_modules/")[1].split("/")[0].toString();
+
+                            if (chunks.includes(module) == false) {
+                                chunks.push(module);
+                            }
+
+                            const number = chunks.indexOf(module) + 1;
+                            const chunkNumber = number > 1 ? number : "";
+
+                            return `chunks/bundle${chunkNumber}`;
+                        }
+                    },
+                },
+            },
+        },
         resolve: {
             alias: {
                 "@": dirname + SRC_DIR,
